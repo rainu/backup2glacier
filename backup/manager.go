08699abefedba5d1083894aa1f7a232e5b93cf77
rainu <rainu@raysha.de>
2018-10-10 19:55:23 +0200

    * implements download module (download and decrypt content)

--- backup/manager.go
+++ backup/manager.go
@@ -4,8 +4,11 @@ import (
 	"backup2glacier/database"
 	"backup2glacier/database/model"
 	"github.com/aws/aws-sdk-go/service/glacier"
+	"github.com/pkg/errors"
 	"io"
+	"os"
 	"sync"
+	"time"
 )
 
 type BackupResult struct {
@@ -18,23 +21,45 @@ type BackupResult struct {
 	Error       error
 }
 
-type BackupManager interface {
+type BackupCreater interface {
 	io.Closer
 
 	Create(file, description, vaultName string) *BackupResult
 }
 
+type BackupGetter interface {
+	io.Closer
+
+	Download(backupId uint, target string) error
+}
+
+type BackupManager interface {
+	io.Closer
+
+	Create(file, description, vaultName string) *BackupResult
+	Download(backupId uint, target string) error
+}
+
 type backupManager struct {
 	dbRepository database.Repository
 	glacier      AWSGlacier
-	crypt        CryptModule
 
 	partSize     int
 	savePassword bool
-	password     string
+	password     *string
+	tier         string
+	pollInterval time.Duration
 }
 
-func NewBackupManager(pw string, savePw bool, partSize int, dbUrl string) (BackupManager, error) {
+func NewBackupCreater(pw string, savePw bool, partSize int, dbUrl string) (BackupCreater, error) {
+	return NewBackupManager(&pw, savePw, partSize, time.Millisecond, "", dbUrl)
+}
+
+func NewBackupGetter(pw *string, tier string, pollInterval time.Duration, dbUrl string) (BackupGetter, error) {
+	return NewBackupManager(pw, false, 0, pollInterval, tier, dbUrl)
+}
+
+func NewBackupManager(pw *string, savePw bool, partSize int, pollInterval time.Duration, tier, dbUrl string) (BackupManager, error) {
 	g, err := NewAWSGlacier()
 	if err != nil {
 		return nil, err
@@ -43,8 +68,9 @@ func NewBackupManager(pw string, savePw bool, partSize int, dbUrl string) (Backu
 	return &backupManager{
 		dbRepository: database.NewRepository(dbUrl),
 		glacier:      g,
-		crypt:        NewCryptModule(pw),
 		partSize:     partSize,
+		pollInterval: pollInterval,
+		tier:         tier,
 		savePassword: savePw,
 		password:     pw,
 	}, nil
@@ -86,6 +112,7 @@ func (b *backupManager) Create(file, description, vaultName string) *BackupResul
 				ZipPath:  content.Zippath,
 				RealPath: content.Realpath,
 				Length:   content.Length,
+				ModTime:  content.FileInfo.ModTime(),
 			})
 		}
 	}()
@@ -95,7 +122,8 @@ func (b *backupManager) Create(file, description, vaultName string) *BackupResul
 		defer wg.Done()
 		defer dstCrypt.Close()
 
-		b.crypt.Encrypt(srcZip, dstCrypt)
+		crypt := NewCryptModule(*b.password)
+		crypt.Encrypt(srcZip, dstCrypt)
 	}()
 
 	var err error
@@ -145,7 +173,7 @@ func (b *backupManager) saveBackupIntent(description string, vaultName string) *
 		Vault:       vaultName,
 	}
 	if b.savePassword {
-		dbBackupEntity.Password = b.password
+		dbBackupEntity.Password = *b.password
 	}
 	b.dbRepository.SaveBackup(dbBackupEntity)
 	return dbBackupEntity
@@ -162,4 +190,82 @@ func (b *backupManager) updateBackup(result *BackupResult, dbBackupEntity *model
 	dbBackupEntity.Length = result.TotalSize
 
 	b.dbRepository.UpdateBackup(dbBackupEntity)
+}
+
+func (b *backupManager) Download(backupId uint, target string) error {
+	fTarget, err := os.Create(target)
+	if err != nil {
+		return errors.Wrap(err, "Could not create target file")
+	}
+	defer fTarget.Close()
+
+	if err != nil {
+		return errors.Wrap(err, "Error while initialise download")
+	}
+
+	toDownload := b.dbRepository.GetBackupById(backupId)
+	if b.password != nil {
+		toDownload.Password = *b.password
+	}
+
+	// glacier -> decrypt -> save as zip
+	srcCrypt, dstCrypt := io.Pipe()
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	var downloadErr error
+	go func() {
+		defer wg.Done()
+		defer dstCrypt.Close()
+
+		downloadErr = b.glacier.Download(AWSGlacierDownload{
+			VaultName:    toDownload.Vault,
+			ArchiveId:    *toDownload.ArchiveId,
+			Target:       dstCrypt,
+			Tier:         b.tier,
+			PollInterval: b.pollInterval,
+		})
+	}()
+
+	var decryptErr error
+	go func() {
+		defer wg.Done()
+
+		crypt := NewCryptModule(toDownload.Password)
+		decryptErr = crypt.Decrypt(srcCrypt, fTarget)
+	}()
+
+	//wait for all to finish
+	wg.Wait()
+
+	if downloadErr != nil {
+		return errors.Wrap(downloadErr, "Error while downloading from glacier")
+	}
+	if decryptErr != nil {
+		return errors.Wrap(decryptErr, "Error while decrypt content")
+	}
+
+	return nil
+}
+
+func (b *backupManager) ensureTarget(target string) error {
+	handle, err := os.Stat(target)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			//if not exists -> create directory
+			if err := os.MkdirAll(target, os.ModePerm); err != nil {
+				return errors.Wrap(err, "Could not create directory")
+			}
+		} else {
+			return errors.Wrap(err, "Could not determine target")
+		}
+	} else {
+		if !handle.IsDir() {
+			return errors.New("Target '" + target + "' is not a directory")
+		}
+	}
+
+	return nil
 }
